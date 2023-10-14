df48a5d6c58b9e161dea9fe479a5def4dd85bf79
Jenelle J Feather <jfeather@mit.edu>
2023-10-14 11:46:55 -0400

    update init

--- robustness/imagenet_models/__init__.py
+++ robustness/imagenet_models/__init__.py
@@ -6,3 +6,13 @@ from .cornet_s import *
 from .hmax import * 
 from .resnet_openselfsup_transfer import * 
 from .vonenet import * 
+
+# Shape trained models
+from .texture_shape_models import *
+
+# For the ipcl models
+from .ipcl_alexnet_gn import ipcl_alexnet_gn
+
+# For VITs, CLIP, and SWSL
+from .vision_transformer import *
+from .timm_resnet_gelu import resnet50_gelu, swsl_resnext101_32x8d
